d8bb1efef9d344bc82cb6b71e187643da6d22d45
Feonik <nikita_k@ur.net>
2014-01-04 23:33:07 +0100

    Пробный запуск BibleQuoteJavaFX с подключенным ядром bqtj

--- src/com/BibleQuote/bqtj/jvm/utils/UpdateManagerJvm.java
+++ src/com/BibleQuote/bqtj/jvm/utils/UpdateManagerJvm.java
@@ -1,16 +1,9 @@
 package com.BibleQuote.bqtj.jvm.utils;
 
-import com.BibleQuote.bqtj.CoreContext;
-import com.BibleQuote.bqtj.utils.DataConstants;
-import com.BibleQuote.bqtj.utils.IUpdateManager;
-import com.BibleQuote.bqtj.utils.Log;
 import com.BibleQuote.bqtj.utils.UpdateManager;
 
-import java.io.*;
 
 public class UpdateManagerJvm extends UpdateManager {
-
-	private final static String TAG = "UpdateManager";
 
 	public UpdateManagerJvm() {
 		super();
